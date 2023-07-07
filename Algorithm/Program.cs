ad91d69dc690ffce78b33f12538314002aff77a7
Javlonbek <122851751+Javlonbek2106@users.noreply.github.com>
2023-07-07 10:09:43 +0500

    Bubble sort added

--- Algorithm/Program.cs
+++ Algorithm/Program.cs
@@ -1,9 +1,15 @@
-﻿namespace Algorithm
+﻿using Algorithm.Sort;
+
+namespace Algorithm
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            foreach (int arg in BubbleSort.SortArray(new int[] {12,1,3,56,25,45,1,2,5,8,9,78 }))
+            {
+                Console.Write(" "+ arg);
+            }
         }
     }
 }
---
+++ Algorithm/Sort/BubbleSort.cs
@@ -0,0 +1,24 @@
+﻿namespace Algorithm.Sort
+{
+    public class BubbleSort
+    {
+        public static int[] SortArray(int[] nums)
+        {
+            int length = nums.Length;
+            for (int i = 0; i < length - 1; i++)
+            {
+                for (int j = 0; j < length - 1 - i; j++)
+                {
+                    if (nums[j] > nums[j + 1])
+                    {
+                        nums[j] = nums[j] + nums[j + 1];
+                        nums[j + 1] = nums[j] - nums[j + 1];
+                        nums[j] = nums[j] - nums[j + 1];
+                    }
+                }
+            }
+            return nums;
+        }
+    }
+}
+
