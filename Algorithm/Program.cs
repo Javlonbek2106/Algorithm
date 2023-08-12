9bd48b48c369076b1fba66429cd82235e84ca461
Javlonbek <122851751+Javlonbek2106@users.noreply.github.com>
2023-08-12 17:20:12 +0500

    binary search added

--- Algorithm/Program.cs
+++ Algorithm/Program.cs
@@ -1,4 +1,5 @@
-﻿using Algorithm.Sort;
+﻿using Algorithm.Search;
+using Algorithm.Sort;
 
 namespace Algorithm
 {
@@ -6,10 +7,12 @@ namespace Algorithm
     {
         static void Main(string[] args)
         {
-            foreach (int arg in BubbleSort.SortArray(new int[] {12,1,3,56,25,45,1,2,5,8,9,78 }))
-            {
-                Console.Write(" "+ arg);
-            }
+            int[] nums = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            //foreach (int arg in BubbleSort.SortArray(new int[] {4,1,2,3,8,4,7,3,0,7,9 }))
+            //{
+            //    Console.Write(" "+ arg);
+            //}
+            Console.WriteLine(BinarySearch.Search(nums,7));
         }
     }
 }
---
+++ Algorithm/Search/BinarySearch.cs
@@ -0,0 +1,25 @@
+﻿namespace Algorithm.Search
+{
+    public class BinarySearch
+    {
+        public static int Search(int[] nums, int num)
+        {
+            int left = 0, right = nums.Length-1;
+            while (left <= right)
+            {
+                int mid = (left + right) / 2;
+                if (nums[mid] == num)
+                {
+                    return mid;
+                }
+                else if (nums[mid] < num)
+                {
+                    left = mid + 1;
+                }
+                else { right = mid + 1; }
+            }
+            return -1;
+        }
+    }
+}
+
